df8cd6eda10ff0d5b7d94bf447f6e8697118c70c
Fajar Muhamad <sptclr.dev@mail.com>
2023-04-24 15:01:48 +0700

    created animated text component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import Layout from "@/components/Layout";
 const inter = Inter({ subsets: ["latin"] });
 
 import profilePic from "../../public/images/profile/developer-pic-1.png";
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -24,12 +27,31 @@ export default function Home() {
                 className="w-full h-auto"
               />
             </div>
-            <div className="w-1/2">
-              <h1>Turning Vision Into Reality With Code And Design.</h1>
-              <p>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="text-left !text-6xl"
+              />
+              <p className="my-4 text-base font-medium">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications.{" "}
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/dummy.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 boder-solid border-transparent hover:border-dark"
+                  // download={true}
+                >
+                  Resume <LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link
+                  href="mailto:abcd@mail.com"
+                  className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
