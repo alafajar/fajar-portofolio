012a8987c9344565ff53ac33d557aa5513150753
Fajar Muhamad <sptclr.dev@mail.com>
2024-05-11 21:32:17 +0700

    finishing portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -48,8 +48,33 @@ const Portfolio = () => {
   return (
     <TopDownPage>
       <div className="h-[600vh]">
-        <div className="w-screen h-[calc(100vh_-_6rem)] flex items-center justify-center text-8xl text-center">
-          My Works!
+        <div>
+          <div className="w-screen h-[calc(100vh_-_6rem)] flex flex-col gap-24 items-center justify-center text-8xl text-center">
+            <span>My Works!</span>
+            {/* Scroll SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={60}
+              height={60}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
+          </div>
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center !overflow-hidden">
           <motion.div style={{ x }} className="flex">
@@ -58,22 +83,57 @@ const Portfolio = () => {
             />
             {items.map((item) => (
               <div
-                key={item.id}
                 className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+                key={item.id}
               >
                 <div className="flex flex-col gap-8 text-white">
-                  <h1>{item.title}</h1>
-                  <div className="relative">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
+                    {item.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
                     <Image src={item.img} alt="" fill />
                   </div>
-                  <p>{item.desc}</p>
-                  <Link href={item.link}>
-                    <button>See Demo</button>
+                  <p className="w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]">
+                    {item.desc}
+                  </p>
+                  <Link href={item.link} className="flex justify-end">
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">
+                      See Demo
+                    </button>
                   </Link>
                 </div>
               </div>
             ))}
           </motion.div>
+        </div>
+      </div>
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-8xl">Do you have a Project?</h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-lg">
+                Front-end-Developer-Front-end-Developer-
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            Hire Me
+          </Link>
         </div>
       </div>
     </TopDownPage>
