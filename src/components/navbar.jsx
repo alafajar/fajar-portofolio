3095160256768d867c0d9493e05093617035d051
Fajar Muhamad <alafajar@gmail.com>
2024-05-11 10:11:56 +0700

    added styling in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,9 @@
+"use client";
+
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
   { url: "/", title: "home" },
@@ -8,11 +12,27 @@ const links = [
   { url: "/contact", title: "Contact" },
 ];
 
+const iconLinks = [
+  { name: "github", url: "#", img: "/github.png" },
+  { name: "dribbble", url: "#", img: "/dribbble.png" },
+  { name: "facebook", url: "#", img: "/facebook.png" },
+  { name: "instagram", url: "#", img: "/instagram.png" },
+  { name: "linkedin", url: "#", img: "/linkedin.png" },
+  { name: "pinterest", url: "#", img: "/pinterest.png" },
+];
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* Links */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
       {/* Logo */}
-      <div>
+      <div className="md:hidden lg:flex lg:w-1/3 lg:justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -23,22 +43,35 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
+      {/* Social */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {iconLinks.map((icon, idx) => (
+          <Link href={icon.url} key={idx}>
+            <Image src={icon.img} alt="" width={24} height={24} />
+          </Link>
+        ))}
+      </div>
       {/* Responsive Menu */}
-      <div>
+      <div className="md:hidden">
         {/* Menu Button */}
-        <button className="w-10 h-8 flex flex-col justify-between">
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <div className="w-10 h-1 bg-black rounded"></div>
           <div className="w-10 h-1 bg-black rounded"></div>
           <div className="w-10 h-1 bg-black rounded"></div>
         </button>
         {/* Menu List */}
-        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-          {links.map((link) => (
-            <Link href={link.url} key={link.title}>
-              {link.title}
-            </Link>
-          ))}
-        </div>
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
