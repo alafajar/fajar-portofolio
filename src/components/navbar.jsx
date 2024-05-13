0e478cf726b9082c0fcb0ff60f754e93e9a33629
Fajar Muhamad <alafajar@gmail.com>
2024-05-13 11:33:09 +0700

    changes name

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -93,9 +93,9 @@ const Navbar = () => {
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
         >
-          <span className="text-white mr-1">Lama</span>
+          <span className="text-white mr-1">Ala</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
-            .dev
+            fajar
           </span>
         </Link>
       </div>
